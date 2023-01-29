45e1cf00459c57d6ffd21d846a30919c1d6cb65a
Josiah Swab <josiah@gdsestimating.com>
2023-01-28 19:42:56 -0800

    init

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    var canvas = document.getElementById("canvas"),
+        context = canvas.getContext("2d"),
+        width = canvas.width = window.innerWidth,
+        height = canvas.height = window.innerHeight;
+
+    for (var i = 0; i < 101; i += 1) {
+        console.log("foo");
+        context.beginPath();
+        context.moveTo(Math.random() * width, Math.random() * height);
+        context.lineTo(Math.random() * width, Math.random() * height);
+        context.stroke();
+    }
+}   
